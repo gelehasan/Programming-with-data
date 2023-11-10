37a4294b67493a2057e63eb7cdae36770c80a9b5
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-10 21:44:35 +0100

    imported neccessary modules

--- handleData.py
+++ handleData.py
@@ -1,1 +1,13 @@
 
+import sys
+sys.executable
+!pip install wordcloud
+
+import nltk
+from nltk.probability import FreqDist
+from nltk.corpus import webtext
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from nltk import sent_tokenize, word_tokenize
+
+nltk.download("punkt")
