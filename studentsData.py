c485f9672b7c700826da079aa74ee4e0b87ebbf4
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-11 19:11:18 +0100

    getting valuable meaning of the data

--- studentsData.py
+++ studentsData.py
@@ -4,3 +4,15 @@ import pandas as pd
 
 df = pd.read_csv("sample.csv")
 
+
+df["Students"].describe()
+
+df.tail(3)
+# You might want to pick some arbitrary slices to explore at more depth.
+df[6:9]
+
+df["Students"].mean()
+
+df["Students"].describe()
+
+df.shape
